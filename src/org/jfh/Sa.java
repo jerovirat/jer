8d6138816c4a3d926175c4b6be1b5f02e73b690f
jerovirat <jerovirat18@gmail.com>
2018-10-25 21:19:32 +0530

    ytgvuyhbi

--- src/org/jfh/Sa.java
+++ src/org/jfh/Sa.java
@@ -4,7 +4,11 @@ public class Sa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("mnhvdjhfv");
+	}
+	private void hgyg() {
+		// TODO Auto-generated method stub
+System.out.println("sdfghjk");
 	}
 
 }
